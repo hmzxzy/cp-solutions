424ac9ab178af4369234879484e2810d0eb7112f
hmzxyz <hamzazouaghii04@gmail.com>
2025-08-17 11:07:53 +0100

    added a bash file to automate pushing files

---
+++ push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Ask for a problem name
+read -p "Enter problem name: " PROBLEM_NAME
+
+# Stage all files in current folder
+git add .
+
+# Commit with simple message
+git commit -m "Solved $PROBLEM_NAME"
+
+# Push to main branch
+git push origin main
+
+echo "Done !"
+
