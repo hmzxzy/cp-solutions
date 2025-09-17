9d678165ea68768323c842cc35c1003f68310e02
hmzxyz <hamzazouaghii04@gmail.com>
2025-09-17 19:55:37 +0100

    Solved A. Vlad and the Best of Five

---
+++ Rt 800/A. Vlad and the Best of Five.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+def solve():
+    t = int(input())
+    for _ in range(t):
+        s = input().strip()
+        a = s.count("A")
+        b = s.count("B")
+        print(a) 
+        print(b)
+        print("A") if a>b else print("B")
+if __name__ == "__main__":
+    solve() 
